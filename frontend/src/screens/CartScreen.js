2d53e43aa458b4d0a4d5bb0d2c31940f35c864b4
Chris <churst19@gmail.com>
2023-08-03 17:24:31 -0400

    more cart changes 8-3-23

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 // import products from "../products"
 import { Row, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
@@ -11,7 +11,8 @@ const CartScreen = () => {
   //save state's quantities to local storage if navigating away
   const [products, setProducts] = React.useState([])
   const [cart, setCart] = React.useState({})
-  let total = 0
+  let total = useRef(0)
+  let productsObject = {}
 
   useEffect(() => {
     console.log("initial effect")
@@ -25,23 +26,15 @@ const CartScreen = () => {
       for (let cartProductId in cart) {
         let temp = await axios.get(`/api/products/${cartProductId}`)
         result.push(temp.data)
+        productsObject[cartProductId] = temp.data
       }
 
       setProducts(result)
     }
     fetchProducts()
+    localStorage.setItem("cart", JSON.stringify(cart))
+
     console.log("products ", products)
-  }, [cart])
-
-  useEffect(() => {
-    console.log("product effect")
-    //copy product array to object
-    // let productsObject = {}
-    // for (let object in products) {
-    //   productsObject[object._id] = object
-    // }
-    // console.log("productsObject ",productsObject)
-
     const calculateTotal = () => {
       let total = 0
 
@@ -58,7 +51,17 @@ const CartScreen = () => {
       }
       return total
     }
-    if (products) total = calculateTotal()
+    if (products) total.current = calculateTotal()
+  }, [cart])
+
+  useEffect(() => {
+    console.log("product effect")
+    //copy product array to object
+    // let productsObject = {}
+    // for (let object in products) {
+    //   productsObject[object._id] = object
+    // }
+    // console.log("productsObject ",productsObject)
   }, [products])
 
   const calculatePrice = (price, qty) => {
@@ -68,37 +71,89 @@ const CartScreen = () => {
     return result
   }
 
+  const handleIncrementItem = (productId) => {
+    // localStorage.setItem("jwt", data.token)
+
+    //get qty of this item
+    let quantity = cart[productId]
+    quantity ? (quantity += 1) : (quantity = 1)
+
+    //update relevant part of cart
+    const result = { [productId]: quantity }
+    setCart((prevState) => {
+      //create new object so state knows something changed in the object
+      let newCart = {}
+      newCart = Object.assign(newCart, cart)
+      newCart = Object.assign(newCart, result)
+      return newCart
+    })
+  }
+
+  const handleDecrementItem = (productId) => {
+    // localStorage.setItem("jwt", data.token)
+
+    //get qty of this item
+    let quantity = cart[productId]
+    if (quantity) quantity -= 1
+
+    //update relevant part of cart
+    const result = { [productId]: quantity }
+    setCart((prevState) => {
+      //create new object so state knows something changed in the object
+      let newCart = {}
+      newCart = Object.assign(newCart, cart)
+      newCart = Object.assign(newCart, result)
+      return newCart
+    })
+  }
+
   const details = products.map((product) => {
     return (
       <div key={product._id} sm={12} md={6} lg={4} xl={3}>
         {/* TODO: add onhover effect similar to header? */}
-        <Link style={{ textDecoration: "none" }} to={`/product/${product._id}`}>
-          <div className="card mb-3" style={{ maxWidth: "540px" }}>
-            <div className="row no-gutters">
-              <div className="col-md-4">
+
+        <div className="card mb-3" style={{ maxWidth: "540px" }}>
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/product/${product._id}`}
+              >
                 <img
                   className="card-img"
                   src={product.image}
                   alt={product.name}
                 />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h6 className="card-title">{product.name}</h6>
-                  <p className="card-text">
-                    ${calculatePrice(product.price, cart[product._id])}
-                  </p>
-                  <p>{product._id}</p>
-                  <p className="card-text">
-                    <small class="text-muted">
-                      REMOVE {cart[product._id]} ADD
-                    </small>
-                  </p>
-                </div>
+              </Link>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h6 className="card-title">{product.name}</h6>
+                <p className="card-text">
+                  ${calculatePrice(product.price, cart[product._id])}
+                </p>
+                <p>{product._id}</p>
+                <p className="card-text">
+                  <small className="text-muted">
+                    <btn
+                      className="button-background"
+                      onClick={() => handleDecrementItem(product._id)}
+                    >
+                      -
+                    </btn>{" "}
+                    {cart[product._id]}{" "}
+                    <btn
+                      className="button-background"
+                      onClick={() => handleIncrementItem(product._id)}
+                    >
+                      +
+                    </btn>
+                  </small>
+                </p>
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       </div>
     )
   })
@@ -107,7 +162,7 @@ const CartScreen = () => {
       <h1>Your Cart</h1>
       <Row>{details}</Row>
       {/* TODO: implement total card on right or below here */}
-      <Row>TOTAL {total}</Row>
+      <Row>TOTAL ${total.current}</Row>
     </div>
   )
 }
