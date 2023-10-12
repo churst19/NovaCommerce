c57b1f7b5eafffaa9392ea79579121ec763f12bb
Chris <churst19@gmail.com>
2023-10-12 12:46:34 -0400

    i

--- server.js
+++ server.js
@@ -24,17 +24,17 @@ app.use(express.json())
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("frontend/build"))
-  // const path = require("path")
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  // })
+  const path = require("path")
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 }
 
 app.get("/", (req, res) => {
   res.send("API is running...")
 })
 app.listen(
-  process.env.PORT || 5000,
+  PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
