7b7693510d47f9dabd01831041167edad6d7edd3
Chris <churst19@gmail.com>
2023-10-12 12:23:15 -0400

    added port from env

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   res.send("API is running...")
 })
 app.listen(
-  5000,
+  process.env.PORT || 5000,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
