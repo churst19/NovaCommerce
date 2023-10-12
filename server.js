65b9929329f6ec0dd9dfd5e6dad4c5301e6519a5
Chris <churst19@gmail.com>
2023-10-12 12:55:27 -0400

    j

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use(express.json())
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("frontend/build"))
-  const path = require("path")
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+  // const path = require("path")
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  // })
 }
 
 app.get("/", (req, res) => {
