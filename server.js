333833e3751ba3e790bf7b1a419f591d4fa7e5a0
Chris <churst19@gmail.com>
2023-10-18 08:43:27 -0400

    troubleshoot stripe production error

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config()
 const FRONTEND_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://novacommerce.onrender.com"
+    : "http://novacommerce.onrender.com"
 
 // "https://novacommerce.onrender.com" || "http://localhost:3000"
 const corsOptions = {
